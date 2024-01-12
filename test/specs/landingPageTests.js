2c477e98745ac2c656f765a30855d0f9b1a4f5b7
Elena Zlateva-Nikolova <elena.zlateva-nikolova.a50@learn.telerikacademy.com>
2024-01-12 16:25:42 +0200

    Test one complete

--- test/specs/landingPageTests.js
+++ test/specs/landingPageTests.js
@@ -7,41 +7,32 @@ describe('Validation of elements on the landing page of https://www.saucedemo.co
     it('should navigate to the site and check if the logo, login button, username and password fields are present', async () => {
         await browser.url('https://www.saucedemo.com/');
 
-
         // Check that Swag Labs text is present
-const pageLogo = await $("//div[@class='login_logo']");
-const logoText = await pageLogo.getText();
-const expectedText = "Swag Labs"; 
-
-assert.equal(logoText, expectedText, "Logo text doesn't match");
+        const pageLogo = await $("//div[@class='login_logo']");
+        const logoText = await pageLogo.getText();
+        const expectedText = "Swag Labs";
+        assert.equal(logoText, expectedText, "Logo text doesn't match");
 
         // Check that username field is present
+        const usernameField = await $("//input[@data-test='username']");
+        const isUsernameFieldDisplayed = await usernameField.isDisplayed();
+        assert.isTrue(isUsernameFieldDisplayed, "Username field is not displayed!")
 
-        e
-        // Check that password fields is present
+        // Check that password field is present
+        const passwordField = await $("//input[@data-test='password']");
+        const isPasswordFieldDisplayed = await passwordField.isDisplayed();
+        assert.isTrue(isPasswordFieldDisplayed, "Password field is not displayed!")
+
         // Check the Login 
+        const loginButton = await $("//input[@data-test='login-button']");
+        const isLoginButtonDisplayed = await loginButton.isDisplayed();
+        assert.isTrue(isLoginButtonDisplayed, "Login button is not displayed!")
+
+
         // Check the credentials info block
+        const infoBlock = await $("//div[@class='login_credentials_wrap-inner']");
+        const isInfoBlockDisplayed = await infoBlock.isDisplayed();
+        assert.isTrue(isInfoBlockDisplayed, "Test login credentials are not displayed!")
 
-
-        // // Locate the context menu
-        // const contextMenu = await $('//div[@id="hot-spot"]');
-
-        // // Right-click on the element
-        // await contextMenu.click({ button: 2, skipRelease: true })
-
-        // // Wait for the alert to appear
-        // await browser.waitUntil(async () => {
-        //     return await browser.isAlertOpen();
-        // });
-
-        // // Validate the alert text 
-        // const alertText = await browser.getAlertText();
-        // assert.equal(alertText, 'You selected a context menu', "The text doesn't match");
-
-        // // Accept the alert
-        // await browser.acceptAlert();
     });
-
-
-
 })
