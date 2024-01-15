6d883d2888d32d047d9a7ffe006afbbbebc510f6
Elena Zlateva-Nikolova <elena.zlateva-nikolova.a50@learn.telerikacademy.com>
2024-01-15 18:38:39 +0200

    Task two completed. Additional assert added to task one, as per the requirements

--- test/specs/loginFormTests.js
+++ test/specs/loginFormTests.js
@@ -2,55 +2,21 @@ import { browser, $ } from '@wdio/globals';
 import { assert } from 'chai';
 
 
-describe('Validation of elements on the landing page of https://www.saucedemo.com/', () => {
-
-    it('should navigate to the site and check if the logo, login button, username and password fields are present', async () => {
-        await browser.url('https://www.saucedemo.com/');
-
-        // Check that Swag Labs text is present
-        const pageLogo = await $("//div[@class='login_logo']");
-        const logoText = await pageLogo.getText();
-        const expectedText = "Swag Labs";
-        assert.equal(logoText, expectedText, "Logo text doesn't match");
-
-        // Check that username field is present
-        const usernameField = await $("//input[@data-test='username']");
-        const isUsernameFieldDisplayed = await usernameField.isDisplayed();
-        assert.isTrue(isUsernameFieldDisplayed, "Username field is not displayed!")
-
-        // Check that password field is present
-        const passwordField = await $("//input[@data-test='password']");
-        const isPasswordFieldDisplayed = await passwordField.isDisplayed();
-        assert.isTrue(isPasswordFieldDisplayed, "Password field is not displayed!")
-
-        // Check the Login 
-        const loginButton = await $("//input[@data-test='login-button']");
-        const isLoginButtonDisplayed = await loginButton.isDisplayed();
-        assert.isTrue(isLoginButtonDisplayed, "Login button is not displayed!")
-        const isLoginButtonEnabled = await loginButton.isEnabled();
-        assert.isTrue(isLoginButtonEnabled, "The login button is disabled!")
-
-
-        // Check the credentials info block
-        const infoBlock = await $("//div[@class='login_credentials_wrap-inner']");
-        const isInfoBlockDisplayed = await infoBlock.isDisplayed();
-        assert.isTrue(isInfoBlockDisplayed, "Test login credentials are not displayed!")
-
-    });
+describe('Validation of elements in the login form on https://www.saucedemo.com/', () => {
 
     it('should check the elements of a registration form and that the default credentials are displayed', async () => {
         await browser.url('https://www.saucedemo.com/');
         const errorMessage = "Text doesn't match!"
 
         // Check the username field’s placeholder/The placeholder is Username
-        const usernamePlaceholder = await $ ("//input[@placeholder='Username']")
+        const usernamePlaceholder = await $("//input[@placeholder='Username']")
         const usernamePlaceholderText = await usernamePlaceholder.getAttribute('placeholder');
-        assert.equal(usernamePlaceholderText, "Username", errorMessage );
+        assert.equal(usernamePlaceholderText, "Username", errorMessage);
 
         // Check that password fields is present/The placeholder is Password
-        const passwordPlaceholder = await $ ("//input[@placeholder='Password']")
+        const passwordPlaceholder = await $("//input[@placeholder='Password']")
         const passwordPlaceholderText = await passwordPlaceholder.getAttribute('placeholder');
-        assert.equal(passwordPlaceholderText, "Password", errorMessage );
+        assert.equal(passwordPlaceholderText, "Password", errorMessage);
 
         // Check the Login button/Text is Login. Color of the button is green (#3ddc91)
         const loginButton = await $("//input[@data-test='login-button']");
@@ -84,7 +50,7 @@ describe('Validation of elements on the landing page of https://www.saucedemo.co
         const password = passwordText[1];
         console.log(password);
         const expectedText = "secret_sauce";
-       
+
         assert.equal(password, expectedText, errorMessage);
 
     });
