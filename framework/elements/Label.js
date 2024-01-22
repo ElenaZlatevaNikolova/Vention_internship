3858c29470aa87726bcae1ad090517793e03c791
Elena Zlateva-Nikolova <elena.zlateva-nikolova.a50@learn.telerikacademy.com>
2024-01-22 11:01:36 +0200

    Wrongly commited framework removed

--- framework/elements/Label.js
+++
@@ -1,9 +0,0 @@
-const BaseElement = require('./BaseElement');
-
-class Label extends BaseElement {
-    constructor(name, selector) {
-        super(name, selector);
-    }
-}
-
-module.exports = Label;
